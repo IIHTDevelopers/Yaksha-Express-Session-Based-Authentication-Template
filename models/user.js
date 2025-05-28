11546734d072cebb8853a46013c4d67bc357300c
IIHT Developers <93914497+IIHTDevelopers@users.noreply.github.com>
2025-05-28 22:12:07 +0530

    Delete models directory

--- models/user.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-    users: [
-        { id: 1, name: 'Admin', email: 'admin@example.com', passwordHash: '$2b$10$ogAyDzVAPyEfhRJ4AJoAouU/GVccB0t2Xgx7vEUBHaIFdfTYCNl5q', age: 30 } // password = 'admin123'
-    ]
-};
